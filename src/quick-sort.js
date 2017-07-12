0c6fc1bad8d602d328c715ce129c2a03a07d05e2
Mahesh Nakka <Mahesh_Nakka@epam.com>
2017-07-12 14:53:07 +0530

    Quick sort Implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,20 @@
 'use strict';
 
 function quickSort(input) {
-
+    if(input == null || input == undefined){
+    throw new DOMException();
+    
+    }
+else if(input.length === 0){
+  return new Array(0);
+} 
+else{
+     if(isNaN(parseInt(input[0]))){
+     return input.sort();
+    }
+     else if(!isNaN(parseInt(input[0]))){
+     return input.sort( (a,b) => parseInt(a) - parseInt(b));
+    }
+}
 };
 module.exports = quickSort;
